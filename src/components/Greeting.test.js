3ab97268df4f8a0849e0bc0da8eb484bb4a7440e
rampion19 <princegupta.pg19@gmail.com>
2023-03-22 15:24:07 +0530

    testing with user action and connected components

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting"
 
 describe('Grreting test', () => {
@@ -13,5 +14,32 @@ describe('Grreting test', () => {
         const helloWorldElement = screen.getByText('Hello World!')
         expect(helloWorldElement).toBeInTheDocument();
 
+    })
+
+    test('renders good to see you if button was not clicked', () => {
+        render(<Greeting />)
+        const outputElement = screen.getByText('good to see you', {exact : false})
+        expect(outputElement).toBeInTheDocument();
+
+    })
+
+    test('renders changed if button was clicked', ()=> {
+        render(<Greeting />)
+
+        const buttonElement = screen.getByRole('button')
+        userEvent.click(buttonElement)
+
+        const outputElement = screen.getByText('Changed!', {exact : false})
+        expect(outputElement).toBeInTheDocument();
+    })
+
+    test('does not render good to see you if button was clicked', () => {
+        render(<Greeting />)
+
+        const buttonElement = screen.getByRole('button')
+        userEvent.click(buttonElement)
+
+        const outputElement = screen.queryByText('good to see you', {exact : false})
+        expect(outputElement).not.toBeInTheDocument();
     })
 })
